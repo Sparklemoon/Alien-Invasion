8643b770fe0af2be5525df13f5b09da586f2d8b4
Myra James <Treesmellgood@gmail.com>
2020-12-13 18:46:57 -0500

    fixed spacehip rofl

--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship:
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship.jpg')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
